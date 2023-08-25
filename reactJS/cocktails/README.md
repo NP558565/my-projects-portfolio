ee1d32b3e07ca9671942408e86cb60b42b45bccd
nanji patel <nanji.patel@btinternet.com>
2023-08-25 15:20:29 +0100

    Folder Upload to Projects added readme

---
+++ reactJS/cocktails/README.md
@@ -0,0 +1,16 @@
+![cocktails](https://user-images.githubusercontent.com/76566329/131918841-b9bdbeb5-bdc9-403b-a491-bd0f9d1f12ff.png)
+Project Title
+The Cocktail Database
+
+Technologies Used:<br>
+React JS<br>, CocktailDB API<br>, React Router<br>, React Hooks.
+
+Installation<br>
+In the project directory, you can run:<br>
+npm start
+
+Runs the app in the development mode.<br>
+Open http://localhost:3000 to view it in the browser.
+
+Link to Demo:<br>
+https://d12k2.csb.app/
---
+++ reactJS/cocktails/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        The Cocktails to provide a free data source api for drinks online in the
+        hope that developers would build applications and cool projects on top
+        of it. The Cocktails originated in one of the forums as a way to browse
+        cocktail recipes on your TV.
+      </p>
+    </section>
+  );
+}
